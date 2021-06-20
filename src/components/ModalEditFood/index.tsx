46e9cc36672f50dc92053dd217dd04f383f2c4f3
hugomatheus <hg.targino@gmail.com>
2021-06-20 01:38:17 -0300

    Desafio - migração: de Javascript para Typescript e de Class Components para Function Components

---
+++ src/components/ModalEditFood/index.tsx
@@ -0,0 +1,59 @@
+import { useRef } from 'react';
+import { FiCheckSquare } from 'react-icons/fi';
+
+import Input from '../Input';
+import Modal from '../Modal';
+import { FormHandles, SubmitHandler } from '@unform/core';
+import { Form } from './styles';
+
+
+interface IFood {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+  price: number;
+  available: boolean;
+}
+
+
+interface ModalEditFoodProps {
+  isOpen: boolean;
+  setIsOpen: () => void;
+  editingFood: IFood;
+  handleUpdateFood: (food: IFood) => Promise<void>;
+}
+
+export default function ModalEditFood({isOpen, setIsOpen, editingFood, handleUpdateFood}: ModalEditFoodProps) {
+
+  const formRef = useRef<FormHandles>(null)
+
+  const handleSubmit: SubmitHandler<IFood> = data => {
+    console.log(formRef)
+    handleUpdateFood(data);
+    setIsOpen();
+  }
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
+        <h1>Editar Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
+
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
+
+        <Input name="description" placeholder="Descrição" />
+
+        <button type="submit" data-testid="edit-food-button">
+          <div className="text">Editar Prato</div>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+}
+
+
